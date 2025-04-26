2a55af317511359b308f7e61db71be53252af878
Damien Sedgwick <damienksedgwick@gmail.com>
2025-04-26 19:34:04 +0100

    added flag for port customisation

--- main.go
+++ main.go
@@ -2,27 +2,32 @@ package main
 
 import (
 	"fem_complete_go/internal/app"
+	"flag"
 	"fmt"
 	"net/http"
 	"time"
 )
 
 func main() {
+	var port int
+	flag.IntVar(&port, "port", 8080, "Port to run the server on")
+	flag.Parse()
+
 	app, err := app.NewApplication()
 	if err != nil {
 		panic(err)
 	}
 
-	app.Logger.Println("we are running our app")
-
 	http.HandleFunc("/health", HealthCheck)
 
 	server := &http.Server{
-		Addr:         ":8080",
+		Addr:         fmt.Sprintf(":%d", port),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
+
+	app.Logger.Printf("we are running on port %d", port)
 
 	err = server.ListenAndServe()
 	if err != nil {
