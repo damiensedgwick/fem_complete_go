11c72eec1fbaf9725e8d41ceb9152f5d366049e4
Damien Sedgwick <damienksedgwick@gmail.com>
2025-04-28 18:48:10 +0100

    fixed migrations issue, app boots

--- internal/store/database.go
+++ internal/store/database.go
@@ -10,7 +10,7 @@ import (
 )
 
 func Open() (*sql.DB, error) {
-	db, err := sql.Open("pgx", "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable")
+	db, err := sql.Open("pgx", "host=localhost user=damiensedgwick password=postgres dbname=postgres port=5432 sslmode=disable")
 	if err != nil {
 		return nil, fmt.Errorf("error opening database: %w", err)
 	}
