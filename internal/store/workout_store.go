3c939c9c805bd516addb4fd5ee74f683484e7a98
Damien Sedgwick <damienksedgwick@gmail.com>
2025-04-29 17:54:57 +0100

    we are back on track!

--- internal/store/workout_store.go
+++ internal/store/workout_store.go
@@ -48,7 +48,7 @@ func (pg *SqliteWorkoutStore) CreateWorkout(workout *Workout) (*Workout, error) 
 
 	query := `
 		INSERT INTO workouts (title, description, duration_minutes, calories_burned)
-		VALUES ($1, $2, $3, $4)
+		VALUES (?, ?, ?, ?)
 		RETURNING id
 	`
 	err = tx.QueryRow(query, workout.Title, workout.Description, workout.DurationMinutes, workout.CaloriesBurned).Scan(&workout.ID)
@@ -59,7 +59,7 @@ func (pg *SqliteWorkoutStore) CreateWorkout(workout *Workout) (*Workout, error) 
 	for _, entry := range workout.Entries {
 		query = `
 			INSERT INTO workout_entries (workout_id, exercise_name, sets, reps, duration_seconds, weight, notes, order_index)
-			VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+			VALUES (?, ?, ?, ?, ?, ?, ?, ?)
 			RETURNING id
 		`
 		err = tx.QueryRow(query, workout.ID, entry.ExerciseName, entry.Sets, entry.Reps, entry.DurationSeconds, entry.Weight, entry.Notes, entry.OrderIndex).Scan(&entry.ID)
